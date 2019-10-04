b24335e2a8d98570af3b2ce5afe73d7d18c19c60
alipro mac <altkowa@gmail.com>
2019-10-05 00:42:48 +0200

    69 حفظ الـ Token  في الآندرويد بلغة الكوتلن

--- app/src/main/java/com/ih/ali/Sign_upActivity.kt
+++ app/src/main/java/com/ih/ali/Sign_upActivity.kt
@@ -2,6 +2,7 @@ package com.ih.ali
 
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class Sign_upActivity : AppCompatActivity() {
 
-
+    var myshered : SharedPreferences? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -46,7 +47,13 @@ class Sign_upActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<RegisterData>, response: Response<RegisterData>) {
                 progressBarsu.visibility = View.GONE
-                Toast.makeText(this@Sign_upActivity,response.body()!!.api_token ,Toast.LENGTH_LONG).show()
+              Toast.makeText(this@Sign_upActivity,response.body()!!.api_token ,Toast.LENGTH_LONG).show()
+                myshered = getSharedPreferences("myshared", 0)
+                var editor:SharedPreferences.Editor = myshered!!.edit()
+                editor.putString("token",response.body()!!.api_token)
+                editor.commit()
+                val it =   Intent(this@Sign_upActivity,welcome::class.java)
+                startActivity(it)
 
             }
 
